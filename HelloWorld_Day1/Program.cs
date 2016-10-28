4fbe9f2ae347104ee4efbae8869af6a0e5590cd7
dianagogoloiu <GDts1903?>
2016-10-28 17:12:13 +0300

    WordPress new commit

---
+++ HelloWorld_Day1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            WordPressPage page = new WordPressPage(" title", "content");
+            WordPressPage page2 = new WordPressPage("2title", "2content");
+
+
+
+          WordPressPage page3 = new WordPressPage(" 3title", " 3content");
+            WordPressPage page4 = new WordPressPage("4title", "4content");
+            WordPressPage page5 = new WordPressPage("4title", "4content");
+            WordPressPage page6 = new WordPressPage("4title", "4content");
+
+           /* page3.isVisible = false;
+            page4.order = 2;
+
+            //page.isVisible = true;
+            Console.WriteLine(page.ToString());
+            Console.WriteLine();
+            Console.WriteLine(page2.ToString());
+            Console.WriteLine();
+            Console.WriteLine(page3.ToString());
+            Console.WriteLine();
+            Console.WriteLine(page4);*/
+            //Console.ReadKey();
+          
+            /*Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();*/
+
+           // page.Publish();
+
+           // Console.WriteLine(page);
+
+          //  Console.ReadKey();
+
+          /*  page.PendingReview();
+            page2.PendingReview();
+            
+            foreach (var i in WordPressPage.lista){
+                Console.WriteLine(i);
+                Console.ReadKey();
+                            }
+            */
+         /*   page3.Publish(5);
+            page4.Publish(DateTime.Now.AddDays(1));
+            page5.SetChild(page4);
+            page6.SetParent(page);
+*/
+            //page.getWordCount();
+
+        //    Console.WriteLine(page.getWordCount());
+          /*  Console.WriteLine();
+            Console.WriteLine(page2);
+            Console.WriteLine();
+            Console.WriteLine(page3);
+            Console.WriteLine();
+Console.WriteLine(page4);
+            Console.WriteLine();
+            Console.WriteLine(page5);
+            Console.WriteLine();
+            Console.WriteLine(page6);*/
+
+            Console.WriteLine(page.isPublished);
+            page.status = WordPressPage.Status.published;
+            page.Publish();
+        Console.WriteLine(page.isPublished);
+           Console.ReadKey();
+        }
+    }
+}
---
+++ HelloWorld_Day1/WordPressPage.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_Day1
+{
+    public class WordPressPage
+    {
+      public  int id;// =2;
+      public string title; //= "sfdsf" ;
+      public string content;//="content test";
+      public enum Status { draft, published, PendingReview };
+      //public Status status = Status.pendingreview;
+
+        public Status status {get; set; } //= Status.PendingReview;
+        public bool isPublished {get {return(this.status==Status.published);}}
+ 
+
+      public  bool isVisible=true;
+      public DateTime PublishDate = DateTime.Now;
+      public int Parent = 1;
+      public int order = 2;
+      public int viewCount = 3;
+
+     public static int PageCount = 0;
+
+
+     public static List<WordPressPage> lista =new List<WordPressPage>();
+        //tostring return an id value
+
+       public override string ToString()
+       {
+           //retrun all the prop
+
+           return id.ToString() + "\ntitle:" + title + "\ncontent: " + content + "\nstatus: " + status + "\nisVisible: " + isVisible + "\nPublishDate: " + PublishDate + "\nParent: " + Parent + "\norder: " + order + "\nviewCount: " + viewCount;
+           //return id.ToString();
+           //return convert.ToString(id);
+       }
+
+
+        public WordPressPage(string title, string content)
+       {
+           this.title = title;
+           this.content = content; 
+            //pagecount - nb pages of wordpress
+           this.id = PageCount++;
+
+            //pagecount++; id=pagecount;
+            
+       }
+
+        //set current date and set status in publish
+
+        public void Publish()
+
+       {
+           try
+           {
+               if (this.isPublished == false)
+               {
+                   throw new Exception();
+               }
+
+           }
+            catch(Exception)
+           {
+               Console.WriteLine("is published");
+           }
+            this.PublishDate = DateTime.Now;
+            this.status = Status.published;
+
+        }
+
+        public void PendingReview()
+        {
+            //pending review ii schimbi statusul si o salvezi in lista
+            //review si pun in lista
+
+            this.status = Status.PendingReview;
+            lista.Add(this);
+        }
+
+
+        
+        public void Publish (int hours){
+            if (this.isPublished == false)
+            {
+                throw new Exception("Is   published");
+            }
+            this.PublishDate = DateTime.Now.AddHours(hours);
+            this.status = Status.published;
+
+    }
+        public void Publish (DateTime datetime)
+        {
+            if (this.isPublished == false)
+            {
+                throw new Exception("Is   published");
+            }
+            this.PublishDate = datetime;
+            this.status = Status.published;
+
+        }
+
+       public void SetChild(WordPressPage page)
+        {
+            //copilul este parametru
+            page.Parent = page.id;
+        }
+
+       public void SetParent(WordPressPage page)
+        {
+            this.Parent = page.id;
+        }
+
+        //nr cuv din content
+
+        public int getWordCount()
+       {
+            //toate cuv sep de un spatiu
+           string[] split = this.content.Split(' ');
+           return split.Length;
+            //return content.Split(' ').Length;
+       }
+
+       
+      
+
+}
+
+    
+
+
+}
